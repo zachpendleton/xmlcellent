f84c34f563b588ae21643b130defc5a36659470e
Zach Pendleton <zachpendleton@gmail.com>
2011-08-19 17:34:45 -0600

    [new] passing tests for define_parser.

--- lib/xmlcellent/parser.rb
+++ lib/xmlcellent/parser.rb
@@ -1,5 +1,20 @@
 module Xmlcellent
   class Parser
+    class << self
+      attr_accessor :formats
 
+      def define_format(name)
+        @formats ||= {}
+        raise "Format already exists!" if @formats.has_key? name
+        @formats[name] = Format.new
+
+        singleton = class << self; self; end
+        singleton.instance_eval do
+          define_method("parse_#{name.to_s}".to_sym) do
+            # method here
+          end
+        end
+      end
+    end
   end
 end
--- test/test_xmlcellent.rb
+++ test/test_xmlcellent.rb
@@ -1,5 +1,26 @@
 require 'helper'
 
 class TestXmlcellent < Test::Unit::TestCase
+  def test_should_create_a_new_parser_on_call_to_define_parser
+    Xmlcellent::Parser.define_format :format_one
+    assert Xmlcellent::Parser.singleton_methods.include? :parse_format_one
+  end
+
+  def test_define_parser_should_create_a_new_format_object
+    Xmlcellent::Parser.define_format :format_two
+    assert Xmlcellent::Parser.formats[:format_two].class == Xmlcellent::Format
+  end
+
+  def test_define_parser_should_add_new_format_to_formats_variable
+    Xmlcellent::Parser.define_format :format_three
+    assert Xmlcellent::Parser.formats.has_key? :format_three
+  end
+
+  def test_define_parser_should_raise_an_error_on_duplicate_key
+    Xmlcellent::Parser.define_format :format_four
+    assert_raise(RuntimeError) do
+      Xmlcellent::Parser.define_format :format_four
+    end
+  end
 
 end
